a17327499ed58af3abf1117a09d7e88462edee39
MaciejWolf <m_wolf@interia.pl>
2020-11-30 22:49:06 +0100

    Add project files.

---
+++ AdventOfCode2020.Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2020.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
